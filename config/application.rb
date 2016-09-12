d9549308e8c863101ef5cd1daeca6f056c3f144c
Yashpal Meena <yaxhpal@gmail.com>
2016-09-12 13:31:55 +0530

    [Yashpal] Changed the fonts directory.

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Xam
     # config.i18n.default_locale = :de
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
